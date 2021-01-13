2f7073f5dfff4ff277072e04711ca2dbcc8b9417
oliviaclyde <olivialclyde@gmail.com>
2021-01-13 09:40:02 -0700

    first cut of unittests

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,33 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVistiorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retreive_it_later(self):
+
+        #User navigates to app homepage
+        self.browser.get('http://localhost:8000')
+        #User views page title and header as to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+        #User enters to-do item
+
+        #User types "buy cookies" into text box
+
+        #User hits enter, pages will update, page will list "1: buy cookies" as an item in a to-do list
+
+        #Text box for input remains. User types another item: "Send cookies for class party"
+
+        #Page updates again, and both items show on to-do list
+
+        #User tests if site will remember the items entered. Site provides unique URL with some text explanation
+
+        #User visits url - list is there
+
+if __name__ == '__main__':
+    unittest.main()
