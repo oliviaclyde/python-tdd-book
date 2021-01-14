a27a6e5dc0b084f07df72279adae43b49ed4f680
oliviaclyde <olivialclyde@gmail.com>
2021-01-13 17:11:40 -0700

    functional test now checks if user can input an item

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVistiorTest(unittest.TestCase):
@@ -14,15 +16,24 @@ class NewVistiorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
         #User views page title and header as to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         #User enters to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
 
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
         #User types "buy cookies" into text box
-
+        inputbox.send_keys('Buy cookies')
         #User hits enter, pages will update, page will list "1: buy cookies" as an item in a to-do list
-
+        inputbox.send_keys('Keys.ENTER')
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: Buy cookies' for row in rows)
+        )
         #Text box for input remains. User types another item: "Send cookies for class party"
-
+        self.fail('Finish the test!')
         #Page updates again, and both items show on to-do list
 
         #User tests if site will remember the items entered. Site provides unique URL with some text explanation
