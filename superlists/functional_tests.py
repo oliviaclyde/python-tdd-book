ba6ebaccbfde01f1548508e69e749741b97476ed
oliviaclyde <olivialclyde@gmail.com>
2021-01-25 11:20:58 -0700

    refactored FT to remove duplication

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -10,6 +10,13 @@ class NewVistiorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, expected_row):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(
+            expected_row, [row.text for row in rows]
+        )
+
     def test_can_start_a_list_and_retreive_it_later(self):
 
         #User navigates to app homepage
@@ -26,19 +33,15 @@ class NewVistiorTest(unittest.TestCase):
         #User hits enter, pages will update, page will list "1: buy cookies" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(3)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        
+        self.check_for_row_in_list_table('1: Buy cookies')
+
         #Text box for input remains. User types another item: "Send cookies for class party"
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Send cookies for class party')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(2)
         #Page updates again, and both items show on to-do list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy cookies', [row.text for row in rows])
-        self.assertIn('2: Send cookies for class party', [row.text for row in rows])
+        self.check_for_row_in_list_table('2: Send cookies for class party')
         #User tests if site will remember the items entered. Site provides unique URL with some text explanation
         self.fail('Finish the test!')
         #User visits url - list is there
